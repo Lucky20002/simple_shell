ad8fed68ce6a935086159af0aca27c8a877b0a32
RCKholophe <khongelani8#gmail.com>
2023-08-22 14:10:32 -0700

    AUTHOR

--- AUTHOR.c
+++ AUTHOR.c
@@ -1,4 +1,4 @@
-#This file lists all the people who have contributed to this repository
+This file lists all the people who have contributed to this repository
 
-Kholophe Rabelani <khongelani8@gmaill.com>
+Kholophe Rabelani <khongelani8@gmail.com>
 Lucky Shabangu <luckyshabangu2000@gmail.com>
