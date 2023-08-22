0443c86ff4679d5e323e4d063cf10fa6490f4e49
Lucky20002 <luckyshabangu2000@gmail.com>
2023-08-22 08:07:24 -0700

    simple shell

--- shell.h
+++ shell.h
@@ -1,22 +1,52 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <signal.h>
+#include <string.h>
 
-#define MAX_COMMAND_LENGTH 1024
+/*================ Shell_Init =================*/
 
-/* Function prototypes */
+int main(int ac, char **av, char **env);
 void prompt(void);
-void handleCtrlC(int signal);
-void handleEOF(char *buffer);
-void exitShell(char **command);
-void freeCommandArray(char **command);
-int _strcmp(const char *s1, const char *s2);
-int _atoi(const char *str);
+void handle(int signals);
+void _EOF(char *buffer);
+void shell_exit(char **command);
+
+/*================ create_child =================*/
+
+void create_child(char **command, char *name, char **env, int cicles);
+int change_dir(const char *path);
+
+/*================ Execute =================*/
+
+void execute(char **command, char *name, char **env, int cicles);
+void print_env(char **env);
+char **_getPATH(char **env);
+void msgerror(char *name, int cicles, char **command);
+
+/*================ Tokening =================*/
+
+char **tokening(char *buffer, const char *s);
+
+/*================ Free Memory =================*/
+
+void free_dp(char **command);
+void free_exit(char **command);
+
+/*================ Auxiliar_Functions =================*/
+
+int _strcmp(char *s1, char *s2);
+unsigned int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+
+/*================ END =================*/
 
 #endif
 
