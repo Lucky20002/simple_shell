6a1253ce4027aac955ce106441e7c8296f8545f2
Lucky20002 <luckyshabangu2000@gmail.com>
2023-08-22 13:43:11 -0700

    shell

---
+++ execute.c
@@ -0,0 +1,114 @@
+#include "shell.h"
+
+/**
+ * execute - execution of command by a function
+ * @command: The pointer to tokenized command
+ * @name: shell's name
+ * @env: The pointer to the environmental variables.
+ * @cycles: Number of executed cycles.
+ * Return: No response
+ */
+void execute(char **command, char *name, char **env, int cycles)
+{
+	char **pathways = NULL, *full_path = NULL;
+	struct stat st;
+	unsigned int i = 0;
+
+	if (_strcmp(command[0], "env") != 0)
+		print_env(env);
+
+	if (stat(command[0], &st) == 0)
+	{
+		if (execve(command[0], command, env) < 0)
+		{
+			perror(name);
+			free_exit(command);
+		}
+	}
+	else
+	{
+		pathways = _getPATH(env);
+		while (pathways[i])
+		{
+			full_path = _strcat(pathways[i], command[0]);
+			i++;
+			if (stat(full_path, &st) == 0)
+			{
+				if (execve(full_path, command, env) < 0)
+				{
+					perror(name);
+					free_dp(pathways);
+					free_exit(command);
+				}
+				return;
+			}
+		}
+		msgerror(name, cycles, command);
+		free_dp(pathways);
+	}
+}
+
+/**
+ * print_env - A function that prints all environmental variables.
+ * @env: The pointer to environmental variables.
+ * Return: Nothing.
+ */
+void print_env(char **env)
+{
+	size_t i = 0, len = 0;
+
+	while (env[i])
+	{
+		len = _strlen(env[i]);
+		write(STDOUT_FILENO, env[i], len);
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+}
+
+/**
+ * _getPATH - A function to get the full value from
+ * environmental variable PATH.
+ * @env: The pointer to environmental variables.
+ * Return: All tokenized pathways for commands.
+ */
+char **_getPATH(char **env)
+{
+	char *pathvalue = NULL, **pathways = NULL;
+	unsigned int i = 0;
+
+	pathvalue = strtok(env[i], "=");
+	while (env[i])
+	{
+		if (_strcmp(pathvalue, "PATH"))
+		{
+			pathvalue = strtok(NULL, "\n");
+			pathways = tokening(pathvalue, ":");
+			return (pathways);
+		}
+		i++;
+		pathvalue = strtok(env[i], "=");
+	}
+	return (NULL);
+}
+
+/**
+ * msgerror - A function that prints message not found.
+ * @name: shell's name
+ * @cycles: Number of cycles.
+ * @command: tokenized command by a pointer
+ * Return: No response
+ */
+void msgerror(char *name, int cycles, char **command)
+{
+	char c;
+
+	c = cycles + '0';
+	write(STDOUT_FILENO, name, _strlen(name));
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, &c, 1);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, command[0], _strlen(command[0]));
+	write(STDOUT_FILENO, ": not found\n", 12);
+}
+
